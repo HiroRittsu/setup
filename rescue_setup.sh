03dbf881b0b487363a0c08598f6fa4e35993c99f
MiglyA <33301907+MiglyA@users.noreply.github.com>
2018-05-24 14:56:23 +0900

    Update rescue_setup.sh

--- rescue_setup.sh
+++ rescue_setup.sh
@@ -16,57 +16,61 @@ echo
 echo "これで大丈夫ですか？(y/n)"
 read ok
 
-#javaインストール
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get -f install
-sudo apt-get install oracle-java8-installer
+if [ $ok = 'y' ];then
+	
+	#javaインストール
+	sudo add-apt-repository ppa:webupd8team/java
+	sudo apt-get update
+	sudo apt-get -f install
+	sudo apt-get install oracle-java8-installer
 
-#gitディレクトリ作成
-mkdir $homedir/git
+	#gitディレクトリ作成
+	mkdir $homedir/git
 
-#gitインストール
-sudo apt-get install git
+	#gitインストール
+	sudo apt-get install git
 
-#新サーバー
-cd $homedir/git
-git clone https://github.com/roborescue/rcrs-server.git
-sudo apt install ant
-cd rcrs-server
-ant clean-all
-ant complete-build
-cd
+	#新サーバー
+	cd $homedir/git
+	git clone https://github.com/roborescue/rcrs-server.git
+	sudo apt install ant
+	cd rcrs-server
+	ant clean-all
+	ant complete-build
+	cd
 
-#ソースコード
-cd $homedir/git
-git clone https://github.com/roborescue/rcrs-adf-sample.git
-cd
+	#ソースコード
+	cd $homedir/git
+	git clone https://github.com/roborescue/rcrs-adf-sample.git
+	cd
 
-#RioneLauncherダウンロード
-if [ $rionelauncher = 'y' ]; then
+	#RioneLauncherダウンロード
+	if [ $rionelauncher = 'y' ]; then
 
-	wget -O RioneLauncher.sh https://raw.githubusercontent.com/MiglyA/bash-rescue/master/RioneLauncher3.00.sh
-	mv RioneLauncher.sh $homedir/git/RioneLauncher.sh
+		wget -O RioneLauncher.sh https://raw.githubusercontent.com/MiglyA/bash-rescue/master/RioneLauncher3.00.sh
+		mv RioneLauncher.sh $homedir/git/RioneLauncher.sh
+
+	fi
+
+	#ねねっちビューアーインストール
+	if [ $rioneviewer = 'y' ]; then
+
+		echo
+		echo "GoogleDriveのInsdtaller.zipをダウンロードしてください。場所は変更しなくても大丈夫です。"
+		echo
+		firefox https://drive.google.com/drive/folders/0B1BfWs2E1JvbbFpvWGtjTGxDeVU
+		echo
+		echo "完了したら何か入力してください。"
+		read wait
+		unzip Installer.zip
+		bash Installer.sh
+
+	fi
+
+	echo
+	echo
+	echo "環境構築は終了しました"
 	
 fi
-
-#ねねっちビューアーインストール
-if [ $rioneviewer = 'y' ]; then
-
-	echo
-	echo "GoogleDriveのInsdtaller.zipをダウンロードしてください。場所は変更しなくても大丈夫です。"
-	echo
-	firefox https://drive.google.com/drive/folders/0B1BfWs2E1JvbbFpvWGtjTGxDeVU
-	echo
-	echo "完了したら何か入力してください。"
-	read wait
-	unzip Installer.zip
-	bash Installer.sh
-	
-fi
-
-echo
-echo
-echo "環境構築は終了しました"
 
 exit 1
