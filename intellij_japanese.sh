e667ea643c4fd04b8f9893ebe9ff166b12866032
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2019-09-08 17:39:57 +0900

    Delete intellij_japanese.sh

--- intellij_japanese.sh
+++
@@ -1,9 +0,0 @@
-INTELLIJ=`find ~/ -type d -name ".*" -prune -o -type f -print | grep "idea.sh" | sed 's@/bin/idea.sh@@g'`
-
-wget http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/build/stable/pleiades.zip
-unzip pleiades.zip -d $INTELLIJ
-echo -Xverify:none >> $INTELLIJ/bin/idea64.vmoptions
-echo -javaagent:$INTELLIJ/plugins/jp.sourceforge.mergedoc.pleiades/pleiades.jar >> $INTELLIJ/bin/idea64.vmoptions
-echo -Xverify:none >> $INTELLIJ/bin/idea.vmoptions
-echo -javaagent:$INTELLIJ/plugins/jp.sourceforge.mergedoc.pleiades/pleiades.jar >> $INTELLIJ/bin/idea.vmoptions
-rm pleiades.zip
